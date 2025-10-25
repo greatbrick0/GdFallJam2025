10332d26e94b82da86c0d43b74a334ce2f5a017b
greatbrick <speharley@gmail.com>
2025-10-25 19:13:15 -0400

    Added Skeleton

---
+++ gdfalljam/Scripts/skeleton.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+
+@export var speed = 100.0
+
+var facingRight: bool = false
+
+func _physics_process(delta):
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		velocity.x = 0
+	else:
+		velocity.x = speed * (1 if facingRight else -1)
+
+	move_and_slide()
