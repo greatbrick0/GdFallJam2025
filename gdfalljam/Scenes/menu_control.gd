1a966e2affdff28459981544955c5aacc392e9a2
conorMaccarthy <conor.maccarthy@ontariotechu.net>
2025-10-25 19:47:23 -0400

    First Few Levels

--- gdfalljam/Scenes/menu_control.gd
+++ gdfalljam/Scenes/menu_control.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/test_level_1.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/level_0.tscn")
 
 
 func _on_exit_button_pressed() -> void:
