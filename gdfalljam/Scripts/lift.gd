23abefc37f5836dd60bcc88d038994d437df8490
greatbrick <speharley@gmail.com>
2025-10-26 07:23:29 -0400

    Added Lift

---
+++ gdfalljam/Scripts/lift.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@export var blockers: int = 0
+@export var positions: Array[Vector2] = []
+var targetPos: Vector2
+@export var travelSpeed: float = 500
+
+func _ready():
+	targetPos = global_position
+
+func AdjustBlockersByBool(newBool: bool) -> void:
+	blockers -= 1 if newBool else -1
+	targetPos = positions[blockers]
+
+func AdjustBlockersByInt(newInt: int) -> void:
+	blockers += newInt
+	targetPos = positions[blockers]
+
+func SetOpen() -> void:
+	blockers = 0
+	targetPos = positions[blockers]
+
+func _physics_process(delta):
+	var dist: float = global_position.distance_to(targetPos)
+	global_position += global_position.direction_to(targetPos) * clampf(travelSpeed * delta, -dist, dist)
