9df544567d8dd5332f04fdbe070269dfe0c8609a
greatbrick <speharley@gmail.com>
2025-10-26 04:37:34 -0400

    Grave Stone Spawns Once

--- gdfalljam/Scripts/grave_stone.gd
+++ gdfalljam/Scripts/grave_stone.gd
@@ -7,6 +7,12 @@ func _on_trigger_area_area_entered(area):
 	call_deferred("AttemptSpawnSkeleton")
 
 func AttemptSpawnSkeleton() -> void:
-	skeleRef = skeleObj.instantiate()
-	get_parent().add_child(skeleRef)
-	skeleRef.global_position = global_position
+	if(skeleRef == null):
+		skeleRef = skeleObj.instantiate()
+		get_parent().add_child(skeleRef)
+		skeleRef.global_position = global_position
+	else:
+		if(not skeleRef.collapsed): return
+		else:
+			skeleRef.global_position = global_position
+			skeleRef.Respawn()
--- gdfalljam/Scripts/skeleton.gd
+++ gdfalljam/Scripts/skeleton.gd
@@ -1,19 +1,33 @@
 extends Actor
 class_name Skeleton
 
+var collapsed: bool = false
+
 func _physics_process(delta):
-	
-	if is_on_wall():
-		facingRight = !facingRight
-	
-	if not is_on_floor():
+	if(not collapsed):
+		if is_on_wall():
+			facingRight = !facingRight
+		
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+			velocity.x = 0
+		else:
+			velocity.x = speed * (1 if facingRight else -1)
+	else:
 		velocity += get_gravity() * delta
 		velocity.x = 0
-	else:
-		velocity.x = speed * (1 if facingRight else -1)
 	
 	HandleMovement()
 
 func GetHit(area: Area2D) -> void:
 	if(area.get_parent() is Player):
-		queue_free()
+		collapsed = true
+		$Visuals/AnimatedSprite2D.visible = false
+		$Visuals/Sprite2D.visible = true
+		$Hitbox.call_deferred("shape_owner_set_disabled", 0, true)
+
+func Respawn() -> void:
+	collapsed = false
+	$Visuals/AnimatedSprite2D.visible = true
+	$Visuals/Sprite2D.visible = false
+	$Hitbox.call_deferred("shape_owner_set_disabled", 0, false)
